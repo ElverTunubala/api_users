5a3d004613b2d2573821ef01980a06fae4ad0a99
cristhian229 <cristhianvasquez99@gmail.com>
2024-08-30 15:21:20 -0500

    creacion capas auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,9 +16,9 @@ import { UsersModule } from './users/users.module';
       database: 'user_database',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
-    }), UsersModule
+    }), UsersModule, AuthModule
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AppModule {}
